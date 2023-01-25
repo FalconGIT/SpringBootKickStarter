ce7f221fab29b5cbb49ce63173fd67608cf2fe46
Avinesh <avineshramah@gmail.com>
2023-01-25 16:25:39 +0000

    [Ticket Number 001] - Adding the new endpoint /getBooks

--- src/main/java/com/prep/springsetup/kickstart/controller/CustomController.java
+++ src/main/java/com/prep/springsetup/kickstart/controller/CustomController.java
@@ -3,11 +3,22 @@ package com.prep.springsetup.kickstart.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class CustomController {
 
     @GetMapping("/test")
     public String test() {
         return "this is the test";
+    }
+
+    @GetMapping("/getBooks")
+    public Map<String, Integer> getBooks(){
+        return new HashMap<String, Integer>() {{
+            put("Book 1", 8);
+            put("Book 2", 37);
+        }};
     }
 }
